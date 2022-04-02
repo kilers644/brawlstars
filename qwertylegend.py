9b48bb953c0db6423c4eae4ab3349fd1a3de441a
kilers644 <102859240+kilers644@users.noreply.github.com>
2022-04-02 19:15:36 +0700

    Update qwertylegend.py

--- qwertylegend.py
+++ qwertylegend.py
@@ -4,13 +4,11 @@ clock = time.Clock()
 okno = display.set_mode((1250,620))
 
 while game:
-#    if musicon == 1:
-#        mixer.music.unpause()
-#    else:
-#        mixer.music.pause()
     for i in event.get():  
         if i.type == QUIT: 
             game = False
+    
+    okno.fill((255,0,0))
 
           
           
