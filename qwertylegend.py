0adcd1afb3e6846381dec8786dd974e1eb6aee97
kilers644 <102859240+kilers644@users.noreply.github.com>
2022-04-02 19:28:14 +0700

    Update qwertylegend.py

--- qwertylegend.py
+++ qwertylegend.py
@@ -3,15 +3,17 @@ game = True
 clock = time.Clock() 
 okno = display.set_mode((1250,620))
 
+color = (196,73,14)
+aboba = Rect(30, 30, 60, 60)
+
+
 while game:
     for i in event.get():  
         if i.type == QUIT: 
             game = False
-    
     okno.fill((255,0,0))
-    
+    draw.rect(okno,color,aboba)
           
           
           
     display.update()
-#
